4981536249e4de847c65d4807096322fd3bccda1
mgomesr <mgomesr.pt@gmail.com>
2023-09-08 20:16:52 -0300

    design home page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GamesListComponent } from './pages/games-list/games-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'games-list',
+    component: GamesListComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardsComponent } from './components/small-cards/small-cards.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GamesListComponent } from './pages/games-list/games-list.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HomeComponent } from './pages/home/home.component';
     SideBarComponent,
     BigCardComponent,
     SmallCardsComponent,
-    HomeComponent
+    HomeComponent,
+    GamesListComponent
   ],
   imports: [
     BrowserModule,
