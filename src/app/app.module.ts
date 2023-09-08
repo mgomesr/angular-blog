c54ae2530af30d3974b5a8aee577892bc2bbd18a
mgomesr <mgomesr.pt@gmail.com>
2023-09-07 22:54:24 -0300

    first commit with the codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule } from '@angular/material/icon'
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SideBarComponent,
+    BigCardComponent,
+    SmallCardsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
